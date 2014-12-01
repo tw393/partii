35617f62fb1e85e8f071b5fdf72104d039f2f899
tom <tw393@cam.ac.uk>
2014-12-01 16:02:24 +0000

    na

--- acceleration_figure.m
+++ acceleration_figure.m
@@ -12,7 +12,7 @@ set(gcf, 'Visible', 'off');
 p1 = plot(Acceleration_plain_droso, 'LineWidth', line_width, 'Color', 'r');
 p2 = plot(Acceleration_plain_killer, 'LineWidth', line_width, 'Color', 'b');
 xlabel({'Frame number'}, 'FontSize', 12, 'FontWeight', 'bold')
-ylabel({'Acceleration of object', '(mm/s)'}, 'FontSize', 12, 'FontWeight', 'bold')
+ylabel({'Acceleration of object', '(mm/s^2)'}, 'FontSize', 12, 'FontWeight', 'bold')
 title('Graph of accelerations of predator and prey', 'FontSize', 12, 'FontWeight', 'bold')
 legend('Acceleration of prey', 'Acceleration of predator', 'location', 'southeast')
 set(gca, 'TickDir', 'out')
