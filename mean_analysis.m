e0ef62c5e234ccb379f50bfbd1e6aacd6f9f373e
tom <tw393@cam.ac.uk>
2014-12-01 13:58:01 +0000

    simplifying for simple master

--- mean_analysis.m
+++ mean_analysis.m
@@ -1,9 +1,13 @@
 %% plots speeds of droso and predator against frame number
 line_width = 2;
+load(uigetfile('distance_analysis.mat'));
+if exist('SpEeD_droso') == 0;
+    return
+end
 %%
-f2 = figure();
+figure('MenuBar', 'none');
 hold on;
-set(f2, 'Visible', 'off');
+set(gcf, 'Visible', 'off');
 p1 = plot(SpEeD_droso, 'LineWidth', line_width, 'Color', 'r');
 p2 = plot(SpEeD_killer, 'LineWidth', line_width, 'Color', 'b');
 xlabel({'Frame number'}, 'FontSize', 12, 'FontWeight', 'bold')
@@ -11,15 +15,15 @@ ylabel({'Speed of object', '(mm/s)'}, 'FontSize', 12, 'FontWeight', 'bold')
 title('Graph of speeds of predator and prey', 'FontSize', 12, 'FontWeight', 'bold')
 legend('Speed of prey', 'Speed of predator', 'location', 'southeast')
 set(gca, 'TickDir', 'out')
-ax = gca;
-[xaf, yaf] = ds2nfu([xa1, xa2], [ya1, ya2]);
 
-set(f2, 'Visible', 'on');
-%% save figure as png and fig
-saveas(f2, 'speed_holco_versus_droso.png', 'png');
-saveas(f2, 'speed_holco_versu_droso.fig', 'fig');
-% pause(9);
-% close()
+set(gcf, 'Visible', 'on');
+%% save figures in single folder
+saveas(gcf, ['F:\analysis\' pngstr1], 'png');
+saveas(gcf, ['F:\analysis\' figstr1], 'fig');
+%% save figures in current folder
+saveas(gcf, figstr1, 'fig');
+saveas(gcf, pngstr1, 'png');
+
 mean_speed_droso = mean(SpEeD_droso);
 mean_speed_killer = mean(SpEeD_killer);
 variance_speed_droso = var(SpEeD_droso);
