94b3b2f97b3490314468fe5cce5bc24204352e95
tom <tw393@cam.ac.uk>
2014-12-01 23:35:24 +0000

    creates matrices of speed/accel data for droso and killer

--- variable_analysis.m
+++ variable_analysis.m
@@ -1,17 +1,37 @@
 list_of_variable_files = dir('*_analysis.mat');
+droso_array = zeros(27,6);
+killer_array = zeros(27,6);
 for k = 1:length(list_of_variable_files);
     load(list_of_variable_files(k).name);
     droso_vars(k) = droso;
     killer_vars(k) = killer;
     clear droso killer
 end
-droso_array = struct2cell(droso_vars);
-killer_array = struct2cell(killer_vars);
-%% cell array
-% cellarray(1, :, x) = mean_speed
-% cellarray(2, :, x) = variance_speed
-% cellarray(3, :, x) = max_speed
-% cellarray(4, :, x) = mean_acceleration
-% cellarray(5, :, x) = variance_acceleration
-% cellarray(6, :, x) = max_acceleration
-%%
+
+for k = 1:length(list_of_variable_files);
+    droso_array(k,1) = droso_vars(k).mean_speed;
+    droso_array(k,2) = droso_vars(k).variance_speed;
+    droso_array(k,3) = droso_vars(k).max_speed;
+    droso_array(k,4) = droso_vars(k).mean_acceleration;
+    droso_array(k,5) = droso_vars(k).variance_acceleration;
+    droso_array(k,6) = droso_vars(k).max_acceleration;
+
+    killer_array(k,1) = killer_vars(k).mean_speed;
+    killer_array(k,2) = killer_vars(k).variance_speed;
+    killer_array(k,3) = killer_vars(k).max_speed;
+    killer_array(k,4) = killer_vars(k).mean_acceleration;
+    killer_array(k,5) = killer_vars(k).variance_acceleration;
+    killer_array(k,6) = killer_vars(k).max_acceleration;
+end
+
+mean_speed_all_droso = mean(droso_array(:,1));
+mean_acceleration_all_droso = mean(droso_array(:,4));
+mean_speed_all_killer = mean(killer_array(:,1));
+mean_acceleration_all_killer = mean(killer_array(:,4));
+%% explanation of variable arrays
+% array(:,1) = mean_speed
+% array(:,2) = variance_speed
+% array(:,3) = max_speed
+% array(:,4) = mean_acceleration
+% array(:,5) = variance_acceleration
+% array(:,6) = max_acceleration
