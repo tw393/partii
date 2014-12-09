8d57c382cff86fc1ba092fad339cd72e3127490a
tom <tw393@cam.ac.uk>
2014-12-09 16:55:05 +0000

    saving file changes

--- distance_figure.m
+++ distance_figure.m
@@ -1,3 +1,8 @@
+%% figure options
+font_name = 'Arial';
+font_size = 12;
+bold_tog = 'bold';
+
 %% get distance between bead and fly at takeoff
 % load each *_distance_analysis.mat
 % save into matrix    c1 = distance to fly at start of head flick
@@ -9,6 +14,7 @@
 %                     c7 = time to catch
 % plot each of each versus distance
 %% get variables
+cd('F:\analysis\analysis_data');
 list = dir('*_distance_analysis.mat');
 %% collate data into one matrix
 for k = 1:length(list);
@@ -33,20 +39,76 @@ for k = 1:length(list);
     Ma(k,7) = killer_catchframe - Killer_1stmovingframe;
     catch message
         fprintf(1,'Pass %d\n%s\n%s\n\n',k, list(k).name, message.message)
-        clearvars('-except', 'k', 'list', 'Ma')
+        clearvars('-except', 'k', 'list', 'Ma', 'font_name', 'font_size', 'bold_tog');
         continue
     end
-    clearvars('-except', 'k', 'list', 'Ma')
+    clearvars('-except', 'k', 'list', 'Ma', 'font_name', 'font_size', 'bold_tog');
 end
 %% create logical index for calculating the mean ignoring zeros
 % because some flies have no data
 logMa = logical(Ma);
-%% c7 vs c3
-index_column = logMa(:,5);
-data_column = Ma(:,5);
+%% what to plot
+query  = {sprintf('4 = duration of head flick\n5 = time for start of flick --> takeoff\n6 = time for end of flick --> takeoff\n7 = time to catch')...
+            , sprintf('1 = distance to fly at start of head flick\n2 = distance at end of head flick\n3 = distance at takeoff')};
+answer = {'answer1', 'answer2'};
+answer = inputdlg(query, 'y vs x');
+answer1 = str2num(answer{1});
+answer2 = str2num(answer{2});
+%% figure labels
+switch answer1
+    case 4
+        yaxis_label = {'Duration of head flick', '/ms'};
+        title1 = yaxis_label{1};
+    case 5
+        yaxis_label = {'Start of head flick to takeoff', '/ms'};
+        title1 = yaxis_label{1};
+
+    case 6
+        yaxis_label = {'End of head flick to takeoff', '/ms'};
+        title1 = yaxis_label{1};
+    case 7
+        yaxis_label = {'Duration of flight', '/ms'};
+        title1 = yaxis_label{1};
+end
+switch answer2
+    case 1
+        xaxis_label = {'Distance to prey','(at start of head flick) /mm'};
+        title2 = xaxis_label{1};
+    case 2
+        xaxis_label = {'Distance to prey','(at end of head flick) /mm'};
+        title2 = xaxis_label{1};
+    case 3
+        xaxis_label = {'Distance to prey','(at takeoff) /mm'};
+        title2 = xaxis_label{1};
+end
+%% y axis
+index_column = logMa(:,answer1);
+data_column = Ma(:,answer1);
 indexed_data = data_column(index_column);
 y = indexed_data;
+%% x axis
+index_column2 = logMa(:,answer2);
+data_column2 = Ma(:,answer2);
+indexed_data2 = data_column2(index_column2);
+x = indexed_data2;
 %% create figure
-f = figure('Visible','off','MenuBar','none');
-p1 = bar(1:length(indexed_data), y);
+f = figure('MenuBar', 'none', 'Position', [360 215 1200 650]);
+hold on;
+set(gcf, 'Visible', 'off');
+p1 = scatter(x, y, 'o', 'r');
+title(sprintf('%s vs. %s', title1, title2), 'FontName', font_name, 'FontSize', font_size, 'FontWeight', bold_tog)
+xlabel(xaxis_label, 'FontName', font_name, 'FontSize', font_size, 'FontWeight', bold_tog)
+ylabel(yaxis_label, 'FontName', font_name, 'FontSize', font_size, 'FontWeight', bold_tog)
 set(f, 'Visible','on');
+%% save figures in current folder
+t = clock();
+figtitle = sprintf('F:/analysis/figures/%.1f_%s_%s.fig', t(6), title1, title2);
+pngtitle = sprintf('F:/analysis/figures/%.1f_%s_%s.png', t(6), title1, title2);
+saveas(gcf, figtitle, 'fig');
+saveas(gcf, pngtitle, 'png');
+
+f = fopen(sprintf('F:/analysis/figures/%.1f flies used.txt', t(6)), 'a+');
+for i = 1:length(list)
+fprintf(f, '%s\n\n', list(i).name);
+end
+close(f);
